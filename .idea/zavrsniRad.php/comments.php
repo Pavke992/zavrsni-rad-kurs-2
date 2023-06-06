0e5ba4b7d0e9d2f05fbb1f7704f6f77342d28e6e
Pavke992 <stefanpavicic992@gmail.com>
2023-06-07 00:48:48 +0200

    novo sta ja znam

--- .idea/zavrsniRad.php/comments.php
+++ .idea/zavrsniRad.php/comments.php
@@ -1,4 +1,42 @@
-<?php include('header.php'); 
-      include('footer.php');
-      include('sidebar.php');
+
+    <!-- List of comments -->
+    <?php
+    // Checking if the post ID was passed
+    if (isset($_GET['id'])) {
+        $postId = $_GET['id'];
+
+        // Prepares SQL queries to retrieve comments for a specific post
+        $query = $conn->prepare('SELECT * FROM comments WHERE Post_id = :postId');
+        $query->bindParam(':postId', $postId);
+        $query->execute();
+
+        //Retrieving query results
+        $comments = $query->fetchAll();
+
+        //Showing all comments
+        foreach ($comments as $comment) {
+            echo '<h3>Autor: ' . $comment['Author'] . '</h3>';
+            echo '<p>' . $comment['Text'] . '</p>';
+            echo '<hr>';
+        }
+    } else {
+        echo 'No post ID found.';
+    }
 ?>
+
+<a href="single-post.php?id=<?php echo $postId; ?>">Return to single-post</a>
+    
+    <!-- Form for adding comments -->
+    <form action="comments.php" method="POST">
+        <input type="hidden" name="post_id" value="<?php echo $postId; ?>">
+        <div>
+            <label for="author">Author:</label>
+            <input type="text" id="author" name="author" required>
+        </div>
+        <div>
+            <label for="comment">Comment:</label>
+            <textarea id="comment" name="comment" required></textarea>
+        </div>
+        <button type="submit">Add Comment</button>
+    </form>
+</div>
--- .idea/zavrsniRad.php/create-post.php
+++ .idea/zavrsniRad.php/create-post.php
@@ -1,65 +1,46 @@
 <?php include('db.php'); ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="shortcut icon" href="favicon.ico">
-    <title>Vivify Academy Blog - Homepage</title>
-
-    <link rel="stylesheet" href="styles/blog.css">
-</head>
 <?php include('header.php'); ?>
-<body class="va-l-page va-l-page--homepage">
 
 <?php
 
-// checking if the form has been submitted
+// Checking if the post creation form has been submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  // Retrieve the form data
+  // Data validation and storage
   $title = $_POST['title'];
-  $author = $_POST['author'];
   $body = $_POST['body'];
-  $created_at = $_POST['created_at'];
+  $author = $_POST['author'];
 
+  //Preparing a SQL query to enter a new post
+  $query = $conn->prepare('INSERT INTO posts (Title, Body, Author) VALUES (:title, :body, :author)');
+  $query->bindParam(':title', $title);
+  $query->bindParam(':body', $body);
+  $query->bindParam(':author', $author);
+  $query->execute();
 
-  // Save the blog post to a database or file, or perform any other necessary actions
-  // For this example, let's just display the submitted data
-  // echo "<h2>Blog Post Created!</h2>";
-  // echo "<p>Title: $title</p>";
-  // echo "<p>Author: $author</p>";
-  // echo "<p>body: $body</p>";
-  // echo "<p>Content: $created_at</p>";
-
-  $sql = "insert into posts (title, author, body, created_at) values ('{$title}', '{$author}', '{$body}', '{$created_at}')";
-  $statement = $connection->prepare($sql);
-  $statement->execute();
-  header("Location: index.php");
+  //Redirection to all posts
+  header('Location: posts.php');
 }
 ?>
 
-<h2>Create a Blog Post</h2>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Blog - Create a post</title>
+</head>
+<body>
+<h1>Blog - Create a post</h1>
 
-<form action="create-post.php" method="POST">
-  <label for="title">Title:</label>
-  <input type="text" id="title" name="title" required>
-
-  <label for="author">Author:</label>
-  <input type="text" id="author" name="author" required>
-
-  <label for="body">Body:</label>
-  <textarea id="body" name="body" required></textarea>
-
-  
-  <label for="created_at">Created At:</label>
-  <textarea id="created_at" name="created_at" required></textarea>
-
-  <input type="submit" value="Create Post">
-
+<form method="POST" action="">
+  <label for="title">Title:</label><br>
+  <input type="text" name="title" id="title"><br><br>
+  <label for="body">Content:</label><br>
+  <textarea name="body" id="body"></textarea><br><br>
+  <label for="author">Author:</label><br>
+  <input type="text" name="author" id="author"><br><br>
+  <input type="submit" value="Create post">
 </form>
-      <?php include('sidebar.php'); ?>
 </body>
+</html>
+      <?php include('sidebar.php'); ?>
       <?php include('footer.php'); ?>
